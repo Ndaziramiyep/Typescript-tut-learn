faeb3bb0ed4aadf195b69af1ce48c0164fc77367
Ndaziramiyep <patrickofficial30@gmail.com>
2024-06-27 21:11:33 +0200

    typescript learning

--- index.ts
+++ index.ts
@@ -1,2 +1,9 @@
 enum Color {Red = 0, Green = 1, Blue = 2}
 let backgroundColor = Color.Red;
+
+// hello.ts
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet("Alice", new Date());
