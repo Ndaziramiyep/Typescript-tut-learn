af67f2bef78206ad74b3a941666fdcc569619971
Ndaziramiyep <patrickofficial30@gmail.com>
2024-06-27 21:13:01 +0200

    ts basics

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
-enum Color {Red = 0, Green = 1, Blue = 2}
-let backgroundColor = Color.Red;
-
 // hello.ts
 function greet(person: string, date: Date) {
     console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 
 greet("Alice", new Date());
+
+// basicTypes.ts
+let isDone: boolean = false;
+let decimal: number = 6;
+let color: string = "blue";
+let list: number[] = [1, 2, 3];
+let tuple: [string, number];
+tuple = ["hello", 10];
+enum Color {Red, Green, Blue}
+let c: Color = Color.Green;
+let notSure: any = 4;
+notSure = "maybe a string instead";
