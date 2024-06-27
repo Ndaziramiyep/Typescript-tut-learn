2f8d431ab91ed27dac1fe66a08bfcb6f7876f443
Ndaziramiyep <patrickofficial30@gmail.com>
2024-06-27 21:14:13 +0200

    ts basics

--- index.ts
+++ index.ts
@@ -16,3 +16,37 @@ enum Color {Red, Green, Blue}
 let c: Color = Color.Green;
 let notSure: any = 4;
 notSure = "maybe a string instead";
+
+// interfaces.ts
+interface User {
+    name: string;
+    age: number;
+}
+
+function getUser(user: User): void {
+    console.log(`Name: ${user.name}, Age: ${user.age}`);
+}
+
+let user: User = { name: "John", age: 25 };
+getUser(user);
+
+// classes.ts
+class Animal {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+class Dog extends Animal {
+    bark() {
+        console.log('Woof! Woof!');
+    }
+}
+
+const dog = new Dog('Rex');
+dog.bark();
+dog.move(10);
