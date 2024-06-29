d7cbb7dd9d39847d5546a8cdbd353a441c1390e9
Ndaziramiyep <patrickofficial30@gmail.com>
2024-06-29 20:45:16 +0200

    adding todo file

---
+++ to-do-list/todo.ts
@@ -0,0 +1,35 @@
+// src/todo.ts
+
+import { Todo } from './types';
+
+export class TodoList {
+  private todos: Todo[] = [];
+  private nextId: number = 1;
+
+  addTodo(text: string): void {
+    const newTodo: Todo = {
+      id: this.nextId++,
+      text,
+      completed: false
+    };
+    this.todos.push(newTodo);
+    console.log(`Added: ${text}`);
+  }
+
+  removeTodo(id: number): void {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+    console.log(`Removed todo with id: ${id}`);
+  }
+
+  toggleTodo(id: number): void {
+    const todo = this.todos.find(todo => todo.id === id);
+    if (todo) {
+      todo.completed = !todo.completed;
+      console.log(`Toggled todo with id: ${id}`);
+    }
+  }
+
+  getTodos(): Todo[] {
+    return this.todos;
+  }
+}
