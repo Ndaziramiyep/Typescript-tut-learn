fbd779bce1578dc8f745b5585e14ebb4bf495f01
Ndaziramiyep <patrickofficial30@gmail.com>
2024-06-30 09:37:53 +0200

    Uploading js files after ts transipilation

--- index.js
+++ index.js
@@ -1,7 +1,72 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+// hello.ts
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+}
+greet("Alice", new Date());
+// basicTypes.ts
+var isDone = false;
+var decimal = 6;
+var color = "blue";
+var list = [1, 2, 3];
+var tuple;
+tuple = ["hello", 10];
 var Color;
 (function (Color) {
     Color[Color["Red"] = 0] = "Red";
     Color[Color["Green"] = 1] = "Green";
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
-var backgroundColor = Color.Red;
+var c = Color.Green;
+var notSure = 4;
+notSure = "maybe a string instead";
+function getUser(user) {
+    console.log("Name: ".concat(user.name, ", Age: ").concat(user.age));
+}
+var user = { name: "John", age: 25 };
+getUser(user);
+// classes.ts
+var Animal = /** @class */ (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 0; }
+        console.log("".concat(this.name, " moved ").concat(distanceInMeters, "m."));
+    };
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Dog.prototype.bark = function () {
+        console.log('Woof! Woof!');
+    };
+    return Dog;
+}(Animal));
+var dog = new Dog('Rex');
+dog.bark();
+dog.move(10);
+//Modules ==============================================================
+// math.ts
+// app.ts
+var math_1 = require("./math");
+console.log((0, math_1.add)(2, 3)); // Output: 5
