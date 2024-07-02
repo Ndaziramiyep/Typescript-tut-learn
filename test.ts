f202b5e932ec5bcc8b9734283b9dfa5040d966bb
Ndaziramiyep <patrickofficial30@gmail.com>
2024-07-02 19:36:06 +0200

    adding parameters into the class's constructor

--- test.ts
+++ test.ts
@@ -6,11 +6,11 @@
 // }
 
 class Student{
-    // id: number;
-    // name:string;
-    // subject_name:string;
-    // subject_code:number;
-    constructor(name="_",subname="Math",subcode=0){
+    id: number;
+    name:string;
+    subject_name:string;
+    subject_code:number;
+    constructor(id:number,name:string,subname:string,subcode:number){
           let id1 = Date.now().toString();
           console.error(name+id1+subname+subcode);
     }
