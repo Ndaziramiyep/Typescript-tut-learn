d33636c7025cb0a382db1ee2819e126781d20301
Ndaziramiyep <patrickofficial30@gmail.com>
2024-07-03 12:24:44 +0200

    Removing element by splice

--- to-do-list/app.ts
+++ to-do-list/app.ts
@@ -34,7 +34,7 @@ class ToDoList {
     }
 
     private removeTask(taskId: number): void {
-        this.tasks = this.tasks.filter(task => task.id !== taskId);
+        this.tasks = this.tasks.splice(taskId,1);
         this.renderTasks();
     }
 
