1b4199841f129d589239ce5f3ba4e3778f756e69
Ndaziramiyep <patrickofficial30@gmail.com>
2024-07-03 15:05:45 +0200

    creating class constructor and initialising variables

--- do.ts
+++ do.ts
@@ -9,5 +9,13 @@ class Action_list{
     private button_element : HTMLButtonElement;
     private list_display : HTMLOListElement;
   
-     
+      constructor(){
+         this.new_record = <HTMLInputElement>document.getElementById("record");
+         this.button_element = <HTMLButtonElement>document.getElementById("btn");
+         this.list_display = <HTMLOListElement>document.getElementById("list");
+  
+         this.button_element.addEventListener("click", this.addTask);
+      }
+  
+       
   }
